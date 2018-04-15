3c24c9b35585a632896e33d7b4a58004c5283894
lilisya <yumaim.lacorp@gmail.com>
2018-04-15 21:53:50 +0700

    footer and remove asset

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="pull-right hidden-xs">
-  <b>Version</b> 2.4.0
+  <b>Version</b> 0.1.2
 </div>
-<strong>Copyright &copy; 2018 <a href="#">Sector Code</a>.</strong> All rights
+<strong>Copyright &copy; 2018 <a href="#">Rentalin.id</a>.</strong> All rights
 reserved.
