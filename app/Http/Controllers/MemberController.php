c1f1a49b1fce7025ee735678f4eefcc5db89ea10
lilisya <yumaim.lacorp@gmail.com>
2018-04-29 19:03:15 +0700

    initambahan guzzle

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class MemberController extends Controller
 {
@@ -23,7 +30,24 @@ class MemberController extends Controller
      */
     public function index()
     {
-    	$items = {};
-        return view('layouts\user',compact('items'));
+$client = new Client();
+$response =     $client->request('get', '/token');
+//         $guzzleClient = new GuzzleHttp\Client(['base_uri' => 'https://api.rentallin.id/']);
+// $client = json_api('v1')->client($guzzleClient);
+
+        // $client = new Client(['base_uri' => 'https://api.rentallin.id/','timeout'  => 2.0,]);
+        // $response =     $client->request('get', '/token');
+
+        // $token =  $response->getBody();
+
+        // $response =     $client->request('POST', '/login',[
+        //                 'headers' => [
+        //                     'X-CSRF-TOKEN' => $token,
+        //                 ]
+        //             ]);
+        // $response = $client->get('https://api.rentallin.id/contacts');
+    	// $client = new Client(); //GuzzleHttp\Client
+     //    $result = $client->get('https://api.rentallin.id/contacts');
+        // return view('layouts\user',compact('items'));
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
-
+require_once __DIR__.'/../vendor/guzzle/autoloader.php';
+// require 'vendor/guzzle/autoloader.php';
 /*
 |--------------------------------------------------------------------------
 | Create The Application
